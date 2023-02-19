e14abe54dfe7e7cf850af114453b074e9514436b
YaroslavTihomolov <93517226+YaroslavTihomolov@users.noreply.github.com>
2023-02-19 19:41:49 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,12 +3,14 @@
 #include <iostream>
 #include <vector>
 
+
 constexpr auto t = 0.00001;
 constexpr auto e_pow_2 = 0.00001 * 0.00001;
 constexpr auto RANK_ROOT = 0;
 constexpr auto N = 15000;
 constexpr auto BREAK = 0;
 constexpr auto CONTINUE = 1;
+
 
 void MatrixMultiply(const double *buf, int lines, const double *x, double *tmp) {
     for (int i = 0; i < lines; i++) {
@@ -40,6 +42,7 @@ std::vector<int> GetlinesCount(int size, int *offset) {
     return lines_count;
 }
 
+
 double Norma(const double *vector, int size) {
     double tmp = 0;
     for (int i = 0; i < size; i++) {
@@ -48,17 +51,20 @@ double Norma(const double *vector, int size) {
     return tmp;
 }
 
-void VectorDifference(const double * a_1, const double *a_2, int size, int offset, double *tmp) {
+
+void VectorDifference(const double *a_1, const double *a_2, int size, int offset, double *tmp) {
     for (int i = offset; i < size + offset; i++) {
         tmp[i - offset] = a_1[i - offset] - a_2[i];
     }
 }
+
 
 void VectorMultiplyConst(const double *a_1, double value, int size, double *tmp) {
     for (int i = 0; i < size; i++) {
         tmp[i] = a_1[i] * value;
     }
 }
+
 
 double GetSummaryNorm(int size, double rank_root_norm) {
     double cur_norma = rank_root_norm;
@@ -72,7 +78,8 @@ double GetSummaryNorm(int size, double rank_root_norm) {
     return cur_norma;
 }
 
-bool NormaCompare(double norma, int size, double norma_b, const std::vector<double>& x) {
+
+bool NormaCompare(double norma, int size, double norma_b, const std::vector<double> &x) {
     if (norma / norma_b < e_pow_2) {
         /*for (auto i: x)
             std::cout << i << " ";*/
@@ -84,14 +91,15 @@ bool NormaCompare(double norma, int size, double norma_b, const std::vector<doub
     return false;
 }
 
-void SendA(const std::vector<int>& offset, const std::vector<int>& lines_count, int rank, std::vector<double>& A_buf) {
+
+void SendA(const std::vector<int> &offset, const std::vector<int> &lines_count, int rank, std::vector<double> &A_buf) {
     std::vector<int> lines_count_mult_N(lines_count.begin(), lines_count.end());
-    for (int & i : lines_count_mult_N) {
+    for (int &i: lines_count_mult_N) {
         i *= N;
     }
 
     std::vector<int> offset_mult_N(offset.begin(), offset.end());
-    for (int & i : offset_mult_N) {
+    for (int &i: offset_mult_N) {
         i *= N;
     }
     if (rank == RANK_ROOT) {
@@ -108,6 +116,7 @@ void SendA(const std::vector<int>& offset, const std::vector<int>& lines_count, 
                      lines_count_mult_N[rank], MPI_DOUBLE, RANK_ROOT, MPI_COMM_WORLD);
     }
 }
+
 
 void Run(int size, int rank) {
     std::vector<int> offset(size);
@@ -172,6 +181,7 @@ void Run(int size, int rank) {
         }
     }
 }
+
 
 int main(int argc, char **argv) {
     int size, rank;
